3674d441d5357fa245d7fc5c195c67c91ef990c0
Mike Williams <mdub@dogbiscuit.org>
2015-05-19 19:03:58 +1000

    Some refactoring.

--- lib/clamp/messages.rb
+++ lib/clamp/messages.rb
@@ -2,35 +2,38 @@ module Clamp
 
   module Messages
 
-    def messages=(messages)
-      @user_defined_messages = messages
+    def messages=(new_messages)
+      messages.merge!(new_messages)
     end
 
     def message(key, options={})
-      @user_defined_messages ||= {}
-      msg = @user_defined_messages[key] || messages[key]
-      format_string(msg, options)
-    end
-
-    def messages
-      {
-        :too_many_arguments => "too many arguments",
-        :option_required => "option '%<option>s' is required",
-        :option_or_env_required => "option '%<option>s' (or env %<env>s) is required",
-        :option_argument_error => "option '%<switch>s': %<message>s",
-        :parameter_argument_error => "parameter '%<param>s': %<message>s",
-        :env_argument_error => "$%<env>s: %<message>s",
-        :unrecognised_option => "Unrecognised option '%<switch>s'",
-        :no_such_subcommand => "No such sub-command '%<name>s'",
-        :no_value_provided => "no value provided",
-        :usage_heading => "Usage",
-        :parameters_heading => "Parameters",
-        :subcommands_heading => "Subcommands",
-        :options_heading => "Options"
-      }
+      format_string(messages.fetch(key), options)
     end
 
     private
+
+    DEFAULTS = {
+      :too_many_arguments => "too many arguments",
+      :option_required => "option '%<option>s' is required",
+      :option_or_env_required => "option '%<option>s' (or env %<env>s) is required",
+      :option_argument_error => "option '%<switch>s': %<message>s",
+      :parameter_argument_error => "parameter '%<param>s': %<message>s",
+      :env_argument_error => "$%<env>s: %<message>s",
+      :unrecognised_option => "Unrecognised option '%<switch>s'",
+      :no_such_subcommand => "No such sub-command '%<name>s'",
+      :no_value_provided => "no value provided",
+      :usage_heading => "Usage",
+      :parameters_heading => "Parameters",
+      :subcommands_heading => "Subcommands",
+      :options_heading => "Options"
+    }
+
+    def messages
+      unless defined?(@messages)
+        @messages = DEFAULTS
+      end
+      @messages
+    end
 
     if (("%{foo}" % {:foo => "bar"}) == "bar")
 
