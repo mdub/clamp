4a9949335074ed645146bb3c18c0ea061a2ec447
Mike Williams <mdub@dogbiscuit.org>
2018-06-17 11:46:56 +1000

    Split complex `#set_options_from_command_line` method.

--- lib/clamp/option/parsing.rb
+++ lib/clamp/option/parsing.rb
@@ -27,40 +27,43 @@ module Clamp #:nodoc:
         argument_buffer = []
         argument_buffer_limit = self.class.parameter_buffer_limit
         until remaining_arguments.empty?
-
           unless remaining_arguments.first.start_with?("-")
             break unless argument_buffer.size < argument_buffer_limit
             argument_buffer << remaining_arguments.shift
             next
           end
-
           switch = remaining_arguments.shift
           break if switch == "--"
-
-          case switch
-          when /\A(-\w)(.+)\z/m # combined short options
-            switch = Regexp.last_match(1)
-            if find_option(switch).flag?
-              remaining_arguments.unshift("-" + Regexp.last_match(2))
-            else
-              remaining_arguments.unshift(Regexp.last_match(2))
-            end
-          when /\A(--[^=]+)=(.*)\z/m
-            switch = Regexp.last_match(1)
-            remaining_arguments.unshift(Regexp.last_match(2))
-          end
-
-          option = find_option(switch)
-          value = option.extract_value(switch, remaining_arguments)
-
-          begin
-            option.of(self).take(value)
-          rescue ArgumentError => e
-            signal_usage_error Clamp.message(:option_argument_error, switch: switch, message: e.message)
-          end
-
+          handle_switch(switch)
         end
         remaining_arguments.unshift(*argument_buffer)
+      end
+
+      def handle_switch(switch)
+        switch = split_trailing_switches(switch)
+        option = find_option(switch)
+        value = option.extract_value(switch, remaining_arguments)
+        begin
+          option.of(self).take(value)
+        rescue ArgumentError => e
+          signal_usage_error Clamp.message(:option_argument_error, switch: switch, message: e.message)
+        end
+      end
+
+      def split_trailing_switches(switch)
+        case switch
+        when /\A(-\w)(.+)\z/m # combined short options
+          switch = Regexp.last_match(1)
+          if find_option(switch).flag?
+            remaining_arguments.unshift("-" + Regexp.last_match(2))
+          else
+            remaining_arguments.unshift(Regexp.last_match(2))
+          end
+        when /\A(--[^=]+)=(.*)\z/m
+          switch = Regexp.last_match(1)
+          remaining_arguments.unshift(Regexp.last_match(2))
+        end
+        switch
       end
 
       def default_options_from_environment
